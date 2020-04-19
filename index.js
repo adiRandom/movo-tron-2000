7924e2e663826cc3ce75d2684d8dc4bcb8456e17
Adrian Pascu <me.adi.hf@gmail.com>
2020-04-20 00:12:18 +0300

    Users

--- index.js
+++ index.js
@@ -4,24 +4,27 @@ const client = new Discord.Client();
 const token = "NzAxNTIxMjQ3MTk2MDIwNzY4.XpyvFw.gbYcrtwLCRflfPgE9wwT4Pe4W7U"
 // const dbl = new DBL(token, client);
 
-const general = "699309436019277945"
+const general =
 
-client.on('message', async (msg) => {
-        //Move mee
-        if (msg.channel.id === general && msg.author.username === 'adi_random' && msg.content.match(new RegExp("[Yy]o+"))) {
-            const channel = await client.channels.fetch('670707818327834655');
-            msg.member.edit({
-                channel
-            }).catch(e => console.log(e))
-        }
+    client.on('message', async (msg) => {
+            //Move mee
+            if (msg.channel.id === general && msg.author.username === 'adi_random' && msg.content.match(new RegExp("[Yy]o+"))) {
+                const channel = await client.channels.fetch('670707818327834655');
+                msg.member.edit({
+                    channel
+                }).catch(e => console.log(e))
+            }
 
-        if (msg.channel.id === general && msg.author.username === 'discpro' && msg.content.match(new RegExp("[Yy]o+"))) {
-            msg.member.edit({
-                    mute: true
-                }
-            )
+            if (msg.channel.id === general && msg.author.username === 'discpro' && msg.content.match(new RegExp("[Yy]o+"))) {
+                msg.member.edit({
+                        mute: true
+                    }
+                )
+            }
+            if (msg.channel.id === general && msg.content === "Taci in mm dragomire") {
+                console.log(client.users);
+            }
         }
-    }
-)
+    )
 
 client.login(token);
