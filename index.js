1965a9872e7d9f661f3fd8af6c62c1e4a78a9f7e
Adrian Pascu <me.adi.hf@gmail.com>
2020-04-20 00:07:17 +0300

    Nob

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ const token = "NzAxNTIxMjQ3MTk2MDIwNzY4.XpyvFw.gbYcrtwLCRflfPgE9wwT4Pe4W7U"
 const general = "699309436019277945"
 
 client.on('message', async (msg) => {
-    //Move mee
-    if (msg.channel.id === general && msg.author.username === 'adi_random' && msg.content.match(new RegExp("[Yy]o+"))) {
-        const channel = await client.channels.fetch('670707818327834655');
-        msg.member.edit({
-            channel
-        }).catch(e => console.log(e))
-    }
+        //Move mee
+        if (msg.channel.id === general && msg.author.username === 'adi_random' && msg.content.match(new RegExp("[Yy]o+"))) {
+            const channel = await client.channels.fetch('670707818327834655');
+            msg.member.edit({
+                channel
+            }).catch(e => console.log(e))
+        }
 
-    if (msg.channel.id === general && msg.author.username === 'discpro' && msg.content.match(new RegExp("[Yy]o+"))) {
-        msg.member.edit({
-            mute: true
+        if (msg.channel.id === general && msg.author.username === 'discpro' && msg.content.match(new RegExp("[Yy]o+"))) {
+            msg.member.edit({
+                    mute: true
+                }
+            )
         }
     }
-})
+)
 
 client.login(token);
