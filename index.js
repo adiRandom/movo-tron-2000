903562e0cc345857a1f35653d8c73764e0fdba26
Adrian Pascu <me.adi.hf@gmail.com>
2020-04-20 00:04:48 +0300

    Nob

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const client = new Discord.Client();
 const token = "NzAxNTIxMjQ3MTk2MDIwNzY4.XpyvFw.gbYcrtwLCRflfPgE9wwT4Pe4W7U"
 // const dbl = new DBL(token, client);
 
+const general = "699309436019277945"
+
 client.on('message', async (msg) => {
-    if (msg.channel.id === '699309436019277945' && msg.author.username === 'adi_random' && msg.content.match(new RegExp("[Yy]o+"))) {
+    //Move mee
+    if (msg.channel.id === general && msg.author.username === 'adi_random' && msg.content.match(new RegExp("[Yy]o+"))) {
         const channel = await client.channels.fetch('670707818327834655');
         msg.member.edit({
             channel
-        }).catch(e=>console.log(e))
+        }).catch(e => console.log(e))
+    }
+
+    if (msg.channel.id === general && msg.author.username === 'discpro' && msg.content.match(new RegExp("[Yy]o+"))) {
+        msg.member.setMute(true);
     }
 })
 
